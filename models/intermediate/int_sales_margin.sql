c28b237d2d491a44902db3aa94eb95d6978b3ded
Benoit Dourdet <benoit.dourdet@gmail.com>
2024-10-30 15:30:40 +0000

    add-on dbt_utils

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,8 +1,13 @@
+WITH sub AS ( 
 SELECT
 sales.*,
 purchase_price,
-sales.quantity * product.purchase_price AS purchase_cost,
-sales.revenue - (sales.quantity * product.purchase_price) AS margin
+ROUND(sales.quantity * product.purchase_price,2) AS purchase_cost,
+ROUND(sales.revenue - (sales.quantity * product.purchase_price),2) AS margin
 FROM {{ ref('stg_raw__sales') }} AS sales 
 LEFT JOIN {{ ref('stg_raw__product') }} AS product
 USING (products_id)
+)
+SELECT *,
+{{ margin_percent("revenue", "purchase_cost",2) }} AS margin_percent
+FROM sub
